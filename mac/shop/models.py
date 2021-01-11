51433b8937fede9576e8bfa12b38fa48d344acf7
hardik <mail4hardikbatra@gmail.com>
2021-01-11 23:21:49 +0530

    Added contact functinality

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    id=models.AutoField
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    msg = models.CharField(max_length=100)
+    date=models.DateField()
--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Contact
+from datetime import datetime
 def index(request):
     return render(request,'shop/index.html')
 
@@ -15,6 +16,12 @@ def store(request):
     return render(request,'shop/store.html')
 
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name','')
+        email=request.POST.get('email','')
+        msg=request.POST.get('msg','')
+        contact=Contact(name=name,email=email,msg=msg,date=datetime.now())
+        contact.save()
     return render(request,'shop/contact.html')
 
 # Create your views here.
